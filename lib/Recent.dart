cd0375cb106a87ccd928bc909d11c3fc58177303
moin <moinkhan603@gmail.com>
2020-03-22 18:41:31 +0500

    icons color changed

--- lib/Recent.dart
+++ lib/Recent.dart
@@ -44,50 +44,60 @@ class _RecentState extends State<Recent> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("RECENT MEMES"),centerTitle: true,
-      backgroundColor: Colors.black87,
+      backgroundColor: Colors.black,
         actions: <Widget>[
           GestureDetector(
               onTap: (){
                 
                goto();
               },
-              child: Icon(Icons.add,size: 35,)),
+              child: Icon(Icons.add,color: Color(0xffF5BD1F),size: 35,)),
         ],
       ),
       
-    body: images!=null?
-        new StaggeredGridView.countBuilder
-          (crossAxisCount: 4
-            ,padding: const EdgeInsets.all(8.0) ,
-          itemCount: images.length,
-          itemBuilder: (context,i){
-             String imgPath=images[i].data['imgUrl'];
+    body: 
+    
+    images!=null?
+        Container(
+          color: Colors.black87,
+          child: new StaggeredGridView.countBuilder
+          
+            (
+              
+              
+              crossAxisCount: 4
+              ,padding: const EdgeInsets.all(10.0) ,
+            itemCount: images.length,
+            itemBuilder: (context,i){
+               String imgPath=images[i].data['imgUrl'];
 
-             return new Material(
-               elevation: 15.0,
-               borderRadius: new BorderRadius.all(new 
-               Radius.circular(15)),
-               child: new InkWell(
-                 onTap:()=> Navigator.push(context,new  MaterialPageRoute(
-             builder: (context)=>FullScreenImagePage(imgPath),
-                 )),
-                 child: new Hero(tag: imgPath, child: new
-                 FadeInImage(placeholder:
-          new AssetImage('assets/images/logo.png')
-                   , image: new
-                 NetworkImage(imgPath),
-                   fit: BoxFit.cover,
-                 )
+               return new Material(
+               
+                 elevation: 15.0,
+                 borderRadius: new BorderRadius.all(new 
+                 Radius.circular(15)),
+                 child: new InkWell(
+                   onTap:()=> Navigator.push(context,new  MaterialPageRoute(
+               builder: (context)=>FullScreenImagePage(imgPath),
+                   )),
+                   child: new Hero(tag: imgPath, child: new
+                   FadeInImage(placeholder:
+            new AssetImage('assets/images/logo.png')
+                     , image: new
+                   NetworkImage(imgPath),
+                     fit: BoxFit.cover,
+                   )
+                   ),
                  ),
-               ),
 
-             );
+               );
 
-          },
+            },
 
-          staggeredTileBuilder:(i)=>new StaggeredTile.count(2, i.isEven?2:3) ,
-          mainAxisSpacing: 5.0,
-          crossAxisSpacing: 3.0,
+            staggeredTileBuilder:(i)=>new StaggeredTile.count(2, i.isEven?2:3) ,
+            mainAxisSpacing: 5.0,
+            crossAxisSpacing: 3.0,
+          ),
         ):new Center(
       child: new CircularProgressIndicator(),
 
